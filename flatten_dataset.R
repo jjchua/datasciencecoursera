422bca2417d4f03db74898241b64a63021ab3959
JJ Chua <jjchua@users.noreply.github.com>
2015-03-23 05:27:33 +1100

    partial: function to merge X and y observations

---
+++ flatten_dataset.R
@@ -0,0 +1,10 @@
+## flatten_dataset.R
+
+## This function creates a flat file from the multiple UCI HAR dataset files
+
+## @featLUT   is the lookup table for feature codes and names
+## @actLUT    is the lookup table for activity codes and names
+
+flatten_dataset <- function(featLUT, actLUT, datadir) {
+  
+}
